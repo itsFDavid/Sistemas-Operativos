94db1cc4904174a4efa788c49df9afbacbd8f696
itsFDavid <fdavid04@icloud.com>
2024-05-25 17:51:28 -0600

    add option to exit or delete rule

--- redes/scripts/blockIp´s.sh
+++ redes/scripts/blockIp´s.sh
@@ -11,3 +11,19 @@ echo "/sbin/iptables -A INPUT -m iprange --src-range $x-$y -p tcp --dport 22 -j 
 /sbin/iptables -A INPUT -m iprange –dst-range $x-$y -p tcp --dport 22 -j DROP
 echo "Rango de ip´s bloqueadas $x - $y"
 echo ""
+
+while true
+	do
+	echo "Desea quitar la regla?"
+	echo "[ 1 ] - Si"
+	echo "[ 2 ] - Salir"
+	read z
+	case $z in
+		1)
+			sudo /sbin/iptables -D INPUT -m iprange –dst-range $x-$y -p tcp --dport 22 -j DROP
+			;;
+		2)
+			exit
+			;;
+	esac
+done
